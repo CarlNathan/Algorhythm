8c3d1a257411f1075d6812c712fad0f7b0b01bc4
CarlNathan <c.n.udren@gmail.com>
2015-12-14 12:52:18 -0500

    Completed project Algorhythm as described in Treehouse tutorial.

--- Algorhythm/PlaylistDetailViewController.m
+++ Algorhythm/PlaylistDetailViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "PlaylistDetailViewController.h"
+#import "Playlist.h"
+
 
 @interface PlaylistDetailViewController ()
 
@@ -16,7 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    if (self.playlist) {
+        self.playlistCoverImage.image = self.playlist.playlistIconLarge;
+        self.playlistCoverImage.backgroundColor = self.playlist.backgroundColor;
+        self.playlistTitle.text = self.playlist.playlistTitle;
+        self.playlistDescription.text = self.playlist.playlistDescription;
+        
+        self.playlistArtist0.text = self.playlist.playlistArtists[0];
+        self.playlistArtist1.text = self.playlist.playlistArtists[1];
+        self.playlistArtist2.text = self.playlist.playlistArtists[2];
+        self.playlistArtist3.text = self.playlist.playlistArtists[3];
+        self.playlistArtist4.text = self.playlist.playlistArtists[4];
+        self.playlistArtist5.text = self.playlist.playlistArtists[5];
+
+
+
+    }
 }
 
 - (void)didReceiveMemoryWarning {
--- Algorhythm/PlaylistMasterViewController.m
+++ Algorhythm/PlaylistMasterViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "PlaylistMasterViewController.h"
+#import "PlaylistDetailViewController.h"
+#import "Playlist.h"
 
 @interface PlaylistMasterViewController ()
 
@@ -16,16 +18,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.aButton setTitle:@"Press me!" forState:UIControlStateNormal];
+    
+    for (NSUInteger index = 0; index < self.playlistImageViews.count; index++) {
+        
+        Playlist *playlist = [[Playlist alloc] initWithIndex:index];
+        
+        UIImageView *playlistImageView = self.playlistImageViews[index];
+        
+        playlistImageView.image = playlist.playlistIcon;
+        playlistImageView.backgroundColor = playlist.backgroundColor;
     }
+    
+    }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)buttonPressed:(id)sender {
-    self.view.backgroundColor = [UIColor orangeColor];
+
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if([segue.identifier isEqualToString:@"showPlaylistDetail"]){
+        
+        UIImageView *playlistImageView = (UIImageView *)[sender view];
+        
+        if ([self.playlistImageViews containsObject:playlistImageView]) {
+            NSUInteger index = [self.playlistImageViews indexOfObject:playlistImageView];
+        
+            PlaylistDetailViewController * playlistDetailController = (PlaylistDetailViewController*) segue.destinationViewController;
+            playlistDetailController.playlist = [[Playlist alloc] initWithIndex:index];
+        
+        
+        }
+    }
 }
+
+
+
+
+- (IBAction)showPlaylistDetail:(id)sender {
+    [self performSegueWithIdentifier:@"showPlaylistDetail" sender:sender];
+}
+
+
+
+
 
 @end
